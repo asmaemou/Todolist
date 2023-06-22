fdadcc74b71b7a9a8e4041feb843bbba072c1ab2
Asmae Mouradi <amouradi2002@gmail.com>
2023-06-22 10:36:38 -0700

    trying to solve local sotrage problem

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
-import React, { useState} from 'react';
+import React, { useState,useEffect} from 'react';
 import Header from "./components/Header";
 import Form from "./components/Form";
 import Todoslist from './components/TodosList';
 
+
 const App = () => {
-  // const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
   const [editTodo, setEditTodo] = useState(null);
 
-  // useEffect(() => {
-  //   localStorage.setItem("todos", JSON.stringify(todos));
-  // }, [todos]);
+  useEffect(() => {
+    // Load todos from local storage on component mount
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Save todos to local storage whenever the todos state changes
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <div className="container">
