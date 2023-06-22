43d196affd9cd0edf508c9155d56acacc550291c
Asmae Mouradi <amouradi2002@gmail.com>
2023-06-21 19:39:12 -0700

    finish the app however i was not able to add useEffect

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,31 +1,45 @@
 import './App.css';
-import React,{useState} from 'react';
+import React, { useState} from 'react';
 import Header from "./components/Header";
 import Form from "./components/Form";
 import Todoslist from './components/TodosList';
 
-const App=()=> {
-  const[input,setInput]=useState("");
-  const[todos,setTodos]=useState([]);
-  return(
-  <div className="container">
-    <div className="app-wrapper">
-      <div>
-        <Header/>
-      </div>
-      <div>
-        <Form
-          input ={input}
-          setInput={setInput}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      </div>
-      <div>
-        <Todoslist todos={todos} setTodos={setTodos}/>
+const App = () => {
+  // const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+
+  // useEffect(() => {
+  //   localStorage.setItem("todos", JSON.stringify(todos));
+  // }, [todos]);
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <Todoslist
+            todos={todos}
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
+        </div>
       </div>
     </div>
-  </div>
   );
 };
+
 export default App;
